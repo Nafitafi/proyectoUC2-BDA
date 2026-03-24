5193a4cc5ba21b2b296e76100c7cce92a92ff252
AndreaLaraOsuna <lara.carmen.cb37@gmail.com>
2026-03-24 16:07:17 -0700

    feat: agregar relación OneToMany entre ClienteFrecuente y Comanda

--- ProyectoUC2-BDA/RestauranteDominio/src/main/java/itson/restaurantedominio/ClienteFrecuente.java
+++ ProyectoUC2-BDA/RestauranteDominio/src/main/java/itson/restaurantedominio/ClienteFrecuente.java
@@ -6,11 +6,13 @@ package itson.restaurantedominio;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -49,6 +51,9 @@ public class ClienteFrecuente implements Serializable {
     @Column(name = "fecha_registro", nullable = false)
     private LocalDate fechaRegistro;
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Comanda> comandas;
+    
     /**
      * Constructor por defecto
      */

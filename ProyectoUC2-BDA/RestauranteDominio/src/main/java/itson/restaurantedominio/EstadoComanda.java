7f6d54925f398148dec6a75f09401c3ac2113c19
AndreaLaraOsuna <lara.carmen.cb37@gmail.com>
2026-03-24 16:07:51 -0700

    feat: agregar enum Estado para el estado de comanda

---
+++ ProyectoUC2-BDA/RestauranteDominio/src/main/java/itson/restaurantedominio/EstadoComanda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package itson.restaurantedominio;
+
+/**
+ *
+ * @author calo2
+ */
+public enum EstadoComanda {
+    ABIERTA, ENTREGADA, CANCELADA
+}
